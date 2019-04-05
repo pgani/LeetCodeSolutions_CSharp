77e04594df01b505a8bf963c9e9933deafb264b7
Peter Gani <peter.gani@live.com>
2019-04-04 22:56:51 -0400

    Two Sum - Submission Complete

---
+++ Interview Prep/Interview Prep/Arrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview_Prep
+{
+    class Arrays
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            //local variables
+            Hashtable indexOfNums = new Hashtable();
+            int remainder;       
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //if the remainder is already in the hashmap, return the indices and the index of the remainder value.
+                remainder = target - nums[i];
+                if (indexOfNums.ContainsKey(remainder))
+                {
+                    if(nums[i] + remainder == target)
+                    {
+                        return new int[] { (int) indexOfNums[remainder], i};
+                    }             
+                }
+                //Otherwise, adds it into the hash table
+                else if(!indexOfNums.ContainsKey(nums[i]))
+                {
+                    indexOfNums.Add(nums[i], i);
+                }
+            }
+
+            //Returns an empty array otherwise.
+            return new int[] { 0, 0 };
+        }
+    }
+}
---
+++ Interview Prep/Interview Prep/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interview_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result = Arrays.TwoSum(nums, target);
+
+            Console.Write("Result is ");
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+        }
+    }
+}
